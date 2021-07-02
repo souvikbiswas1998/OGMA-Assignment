9ff87bd9a5f871d7a39fbaec287bbe64f4ab5051
Souvik Biswas <souvik.biswas1998@gmail.com>
2021-07-02 19:25:24 +0530

    all structure done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

8e358295112e0f32e909da8e731d30cc9f855761
Souvik Biswas <souvik.biswas1998@gmail.com>
2021-07-21 17:34:10 +0530

    small fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -198,7 +198,6 @@ export class AuthService {
   async reauthenticate(password: string): Promise<firebase.auth.UserCredential> {
     return this.afAuth.currentUser.then(user => {
       if (!user) { Promise.reject(new Error('User is not logged in.')); }
-      console.log(user.email, password);
       const credentials = firebase.auth.EmailAuthProvider.credential(user.email, password);
       return user.reauthenticateWithCredential(credentials);
     });
